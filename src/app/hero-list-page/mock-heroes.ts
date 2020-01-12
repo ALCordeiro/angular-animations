b57c87b1ed4e6f31b2071b1468cedc712c739bc5
ALCordeiro <acordeiro@daitan.com>
2020-01-12 20:31:41 -0300

    animations

---
+++ src/app/hero-list-page/mock-heroes.ts
@@ -0,0 +1,10 @@
+
+export const HEROES: any[] = [
+  { id: 1, name: 'Tchuco' },
+  { id: 2, name: 'Waltinho' },
+  { id: 3, name: 'Dennis DJ' },
+  { id: 4, name: 'Rogorfo' },
+  { id: 5, name: 'Marco Desculpinha' },
+  { id: 6, name: 'VITÃO BAGUNCINHA' },
+  { id: 7, name: 'Guilherme 125 Cilindradas' }
+];
