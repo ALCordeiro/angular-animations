f51d5ecd287b979e4e6672e83673d90f9997791a
ALCordeiro <acordeiro@daitan.com>
2020-01-12 20:30:49 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OpenCloseComponent } from './open-close/open-close.component';
+import { StatusSliderComponent } from './status-slider/status-slider.component';
+import { HeroListPageComponent } from './hero-list-page/hero-list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpenCloseComponent,
+    StatusSliderComponent,
+    HeroListPageComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
